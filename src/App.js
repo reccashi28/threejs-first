be5c675048f3eba67fd699ec0157bb53d06c1d04
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-03-15 18:12:44 +0100

    created box and installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Canvas, useFrame } from '@react-three/fiber';
+
+const SpinningMesh = ({ position }) => {
+  const mesh = useRef(null);
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return (<mesh ref={mesh} position={position}>
+    <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+    <meshStandardMaterial attach={'material'} color='lightblue' />
+  </mesh>)
+}
+
 function App() {
+
   return (
-    <div className="App">
-      three js
-    </div>
+    <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+      <ambientLight intensity={.3} />
+      <SpinningMesh position={[0,1,0]} />
+      <SpinningMesh position={[-2,1,-5]} />
+      <SpinningMesh position={[5,1,-2]} />
+    </Canvas>
   );
 }
 
