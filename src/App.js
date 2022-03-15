003f33a65f3a7d3f0a25e1ebda0ca9f330619b21
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-03-15 18:18:05 +0100

    added pointlight

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 import { Canvas, useFrame } from '@react-three/fiber';
 
-const SpinningMesh = ({ position }) => {
+const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
 
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (<mesh ref={mesh} position={position}>
-    <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-    <meshStandardMaterial attach={'material'} color='lightblue' />
+    <boxBufferGeometry attach='geometry' args={args} />
+    <meshStandardMaterial attach={'material'} color={color} />
   </mesh>)
 }
 
@@ -19,9 +19,11 @@ function App() {
   return (
     <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
       <ambientLight intensity={.3} />
-      <SpinningMesh position={[0,1,0]} />
-      <SpinningMesh position={[-2,1,-5]} />
-      <SpinningMesh position={[5,1,-2]} />
+      <pointLight position={[-10, 0,-20]} intensity={.5} />
+      <pointLight position={[0, -10,0]} intensity={1.5} />
+      <SpinningMesh position={[0,1,0]} args={[3,2,1]} />
+      <SpinningMesh position={[-2,1,-5]}  color="pink"  />
+      <SpinningMesh position={[5,1,-2]} color="pink" />
     </Canvas>
   );
 }
